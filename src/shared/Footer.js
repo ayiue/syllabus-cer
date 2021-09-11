7007333eebd655ab33fd4b0a00b218d05aa8aa8a
ayiue <90487715+ayiue@users.noreply.github.com>
2021-09-11 15:47:39 +0800

    New Footer

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -6,9 +6,10 @@ const {Footer}=Layout;
 export function FooterComp(props) {
     return (
         <Footer style={{textAlign: 'center', backgroundColor: 'transparent'}}>
-            基于&nbsp;
+            原作者&nbsp;<a href="https://github.com/xmcp" target="_blank">@xmcp</a>&nbsp;·&nbsp;基于
+            <a href="https://github.com/pkuhelper-web/syllabus" target="_blank">原项目</a>修改后以&nbsp;
             <a href="https://www.gnu.org/licenses/gpl-3.0.zh-cn.html" target="_blank">GPLv3</a>
-            &nbsp;协议在 <a href="https://github.com/pkuhelper-web/syllabus" target="_blank">GitHub</a> 开源
+            &nbsp;协议在 <a href="https://github.com/ayiue/syllabus-cer/" target="_blank">GitHub</a> 开源
         </Footer>
     );
 }
